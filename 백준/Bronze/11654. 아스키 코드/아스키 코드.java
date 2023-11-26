e8c559bde12bef3cc126b0461dcc5157ea4cfa1f
장소윤 <105274058+jangsoyoon@users.noreply.github.com>
2023-11-27 08:33:27 +0900

    [Bronze V] Title: 아스키 코드, Time: 208 ms, Memory: 17592 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.java
@@ -0,0 +1,22 @@
+import java.awt.*;
+import java.io.*;
+import java.util.Scanner;
+
+public class Main {
+//문제
+//알파벳 소문자, 대문자, 숫자 0-9중 하나가 주어졌을 때, 주어진 글자의 아스키 코드값을 출력하는 프로그램을 작성하시오.
+//
+//입력
+//알파벳 소문자, 대문자, 숫자 0-9 중 하나가 첫째 줄에 주어진다.
+//
+//출력
+//입력으로 주어진 글자의 아스키 코드 값을 출력한다.
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.next().charAt(0);
+
+        System.out.println(a);
+    }
+}
