dc4ce071d8d2cc761fcadc1ed612f6b67eecc1c8
장소윤 <105274058+jangsoyoon@users.noreply.github.com>
2024-01-03 10:52:32 +0900

    [Bronze V] Title: 행렬 덧셈, Time: 256 ms, Memory: 19760 KB -BaekjoonHub

--- 백준/Bronze/2738. 행렬 덧셈/행렬 덧셈.java
+++ 백준/Bronze/2738. 행렬 덧셈/행렬 덧셈.java
@@ -1,55 +1,47 @@
-import java.awt.*;
 import java.io.*;
-import java.util.*;
-import java.util.List;
+import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedReader bfr = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bfw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        StringTokenizer st;
-        st = new StringTokenizer(br.readLine(), " ");
+        String str = bfr.readLine();
+        StringTokenizer st = new StringTokenizer(str, " ");
+
         int N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
-        Map<Integer, List<Integer>> aMap = new HashMap<>();
-        Map<Integer, List<Integer>> bMap = new HashMap<>();
+
+        int A[][] = new int[N][M];
+        int B[][] = new int[N][M];
+        int newArr[][] = new int[N][M];
+
         for (int i = 0; i < N; i++) {
-            List<Integer> aList = new ArrayList<>();
-            st = new StringTokenizer(br.readLine(), " ");
+            String strr = bfr.readLine();
+            StringTokenizer stt = new StringTokenizer(strr, " ");
             for (int j = 0; j < M; j++) {
-                aList.add(Integer.parseInt(st.nextToken()));
+                A[i][j] = Integer.parseInt(stt.nextToken());
             }
-            aMap.put(i, aList);
-        }
-        for (int i = 0; i < N; i++) {
-            List<Integer> bList = new ArrayList<>();
-            st = new StringTokenizer(br.readLine(), " ");
-            for (int j = 0; j < M; j++) {
-                bList.add(Integer.parseInt(st.nextToken()));
-            }
-            bMap.put(i, bList);
         }
 
-            A:for (Map.Entry<Integer, List<Integer>> aListEntry : aMap.entrySet()) {
-                for (Map.Entry<Integer, List<Integer>> bListEntry : bMap.entrySet()) {
-                    if (aListEntry.getKey().equals(bListEntry.getKey())) {
-                        int count = 0;
-                        a : for (Integer a : aListEntry.getValue()) {
-                            for (int i = count; i < bListEntry.getValue().size(); i++) {
-                                aListEntry.getValue().set(i, a+bListEntry.getValue().get(i));
-                                count++;
-                                break;
-                            }
-                        }
-                        continue A;
-                    }
-                }
+        for (int i = 0; i < N; i++) {
+            String strr = bfr.readLine();
+            StringTokenizer stt = new StringTokenizer(strr, " ");
+            for (int j = 0; j < M; j++) {
+                B[i][j] = Integer.parseInt(stt.nextToken());
             }
-        for (Map.Entry<Integer, List<Integer>> integerListEntry : aMap.entrySet()) {
-            for (Integer i : integerListEntry.getValue()) {
-                System.out.print(i+" ");
-            }
-            System.out.println();
         }
+
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < M; j++) {
+                newArr[i][j] = A[i][j] + B[i][j];
+                bfw.write(String.valueOf(newArr[i][j] + " "));
+            }
+            bfw.write("\n");
+        }
+
+        bfr.close();
+        bfw.flush();
+        bfw.close();
     }
 }
